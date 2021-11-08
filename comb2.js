c9c727ecbe58e6a78dcb14943fbfa92869454219
Junaid Firdosi <theiconsting17@gmail.com>
2021-11-08 10:48:18 +0530

    comb2 added

---
+++ comb2.js
@@ -0,0 +1,34 @@
+function possibilities(num) {
+  var i = 1
+  console.log(`number ${i} will be adding ${i}, ${num} times`)
+  for (i; i < num - 1; i++) {
+    console.log(`number ${i + 1} is:`)
+    j = i
+    numSumArr = []
+    do {
+      numSumArr.push(1)
+      j++
+    } while (j < num)
+    while (numSumArr.reduce((a, b) => a + b, 0) != num) {
+      numSumArr[0] = numSumArr[0] + 1
+    }
+    console.log("possibility ", ...numSumArr, ` =${num}`)
+    var cloneArr = []
+    var highest = cloneArr[0]
+    for (let index = 0; index < numSumArr.length; index++) {
+      if (index % 2 == 0) {
+        cloneArr.push([numSumArr[index], numSumArr[index + 1]])
+      }
+    }
+    console.log(cloneArr)
+    cloneArr.forEach(el => {
+      if (el[1] == undefined) el[1] = 0
+      el = [el[0] + el[1]]
+      console.log(el)
+    })
+
+    console.log("--end--")
+  }
+}
+
+possibilities(7)
